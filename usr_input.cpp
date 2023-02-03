928c8b96798dfaa84d7f3d54d76d14fd95cf9dac
Dukula <dutandrew78@gmail.com>
2023-02-03 17:24:20 +0300

    a simple c++ program that prints out the age of a person

---
+++ usr_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    /*
+     *A simple program that prints out year of a person
+     */
+    int age = 22;
+    cout << "Your are " << age <<" years old\n";
+    
+    return (0);
+}
